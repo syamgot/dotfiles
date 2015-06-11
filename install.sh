aab24987b6cd586e4b5aa447883efcf9158767aa
syamgot <syamgot@gmail.com>
2015-06-11 10:48:51 +0900

    add

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+
+cd $HOME/dotfiles
+
+#
+if [ ! -d '.vim/bundle' ]; then
+	mkdir -p .vim/bundle
+fi
+git clone https://github.com/Shougo/neobundle.vim .vim/bundle/neobundle.vim
+
+
+# 
+if [ ! -d 'colors' ]; then
+	mkdir -p colors
+fi
+git clone https://github.com/seebi/dircolors-solarized colors/dircolors-solarized
+
+
+. ./link.sh
+
