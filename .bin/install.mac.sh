eff29527b81dea57776a09b11f5dc02946bb389c
masgoto <masgoto@yahoo-corp.jp>
2020-09-29 22:55:52 +0900

    mv mv2trash install script.

--- .bin/install.mac.sh
+++ .bin/install.mac.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 
-# setup
-# ----------------------------------------
-
+# get brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+# brew install
 brew update & install \
 git \
 vim \
@@ -17,5 +16,13 @@ bpctl \
 
 if [ -f $(brew --prefix)/share/zsh/site-functions/_bpctl ]; then
   bpctl completion zsh > $(brew --prefix)/share/zsh/site-functions/_bpctl
+fi
+
+
+# mv2trash
+if [ ! -f ~/dotfiles/bin/mv2trash ]; then
+	echo "download mv2trash!"
+	curl -L https://raw.githubusercontent.com/dankogai/osx-mv2trash/master/bin/mv2trash > ~/dotfiles/bin/mv2trash
+	chmod a+x ~/dotfiles/bin/mv2trash
 fi
 
