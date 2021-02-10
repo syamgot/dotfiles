ba10df5c8196b60fcce062f693a571d93389ff48
syamgot <syamgot@gmail.com>
2021-02-10 22:24:31 +0900

    fix path.

--- .zsh/.zsh.d/00_path.zsh
+++ .zsh/.zsh.d/00_path.zsh
@@ -4,8 +4,8 @@ path=(
 	/usr/local/Cellar/vim/current/bin(N-/)
 	/usr/local/Cellar/git/current/bin(N-/)
 	/usr/local/Cellar/python@3.8/3.8.5/bin(N-/)
-	$HOME/.nodebrew/current/bin(N-/)
-	$HOME/bin
+	~/.nodebrew/current/bin(N-/)
+	~/dotfiles/bin
     $path
 )
 
